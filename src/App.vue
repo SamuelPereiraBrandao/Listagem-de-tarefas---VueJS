756f121449047fa160d73f57db969dbc4adc8a0c
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-27 16:19:17 -0300

    index

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
+            <pre>{{$store.state.todos}}</pre>
+            <TodoSpinner></TodoSpinner>
+            <TodoFormAdd></TodoFormAdd>
+            <TodoItems></TodoItems>
+            <TodoEmpty></TodoEmpty>
+
+
+
+        </div>
+    </div>
+
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import TodoSpinner from '@/components/TodoSpinner.vue'
+import TodoFormAdd from './components/TodoFormAdd.vue';
+import TodoItems from '@/components/TodoItems.vue'
+import TodoEmpty from './components/TodoEmpty.vue'
+import axios from 'axios'
+export default {
+    name: 'PrincipalVue',
+    components: {
+        TodoSpinner,
+        TodoFormAdd,
+        TodoItems,
+        TodoEmpty
+    },
+    created() {
+        axios.get('http://localhost:3000/todos')
+        .then((response) => {
+            this.$store.commit('storeTodos', response.data)
+        })
+    }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/components/TodoItems.vue
@@ -0,0 +1,16 @@
+<template>
+    <TodoItem v-for="todo in $store.state.todos" :key="todo.id">
+
+    </TodoItem>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+export default {
+    name: 'TodoItems',
+    components: {
+        TodoItem,
+
+    }
+}
+</script>
